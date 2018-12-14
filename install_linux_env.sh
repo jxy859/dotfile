283228e716b00a2cd36b8bad893f2199d4467277
jxy859 <jxy859@qq.com>
2018-12-14 11:05:33 +0800

    add sh .vim/pathogen.sh

--- install_linux_env.sh
+++ install_linux_env.sh
@@ -4,7 +4,7 @@
 #   Filename: install_mac_env.sh
 #   Author: Xinyang Jiang <Email:jxy859@gmail.com | Work-Email xyjiang@freewheel.tv>
 #   Create: 2018-11-30
-#   Update: 2018-12-14 10:56:46
+#   Update: 2018-12-14 11:04:59
 #   Desc: 
 #================================================================
 
@@ -14,6 +14,8 @@ for i in .ssh .vim .bash_profile .bashrc .zshrc .vimrc .tmux.conf vimwiki
 do
    ln -s ${workdir}/${i} ~/$i
 done
+
+sh ~/.vim/pathogen.sh
 
 #install home brew
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
