2d8fcd7d451098a1e90e2fb5bb923507617e8926
Xinyang Jiang <xyjiang@Xinyangs-MacBook-Pro.local>
2021-05-25 12:35:14 -0400

    add colors

--- install_mac_env.sh
+++ install_mac_env.sh
@@ -4,21 +4,20 @@
 #   Filename: install_mac_env.sh
 #   Author: Xinyang Jiang <Email:jxy859@gmail.com | Work-Email xyjiang@freewheel.tv>
 #   Create: 2018-11-30
-#   Update: 2021-05-23 22:20:45
+#   Update: 2021-05-24 11:38:31
 #   Desc: 
 #================================================================
 
 workdir=$(pwd)
-
-for i in .ssh .vim .zshrc .vimrc .tmux.conf vimwiki
-do
-   ln -s ${workdir}/${i} ~/$i
-done
-
-
 echo install home brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo install oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /bin/zsh
 brew install tmux
+brew install toilet fortune cowsay
+
+for i in .ssh .vim .zshrc .vimrc .tmux.conf vimwiki
+do
+   ln -s ${workdir}/${i} ~/$i
+done
